00bf86fcf9d85a8f51999e82f7030cb8362f65b0
Jesus Avila <avijesus13@gmail.com>
2024-04-07 09:19:51 -0400

    fixes skeleton

--- src/components/Skeleton.jsx
+++ src/components/Skeleton.jsx
@@ -1,6 +1,6 @@
 const Skeleton = () => (
     <div role="status">
-        <div className="container px-40 py-8 mx-auto animate-pulse">
+        <div className="container px-2 lg:px-40 py-8 mx-auto animate-pulse">
             <div className="grid grid-cols-1 gap-8 mt-8 xl:mt-12 xl:gap-12 sm:grid-cols-2 xl:grid-cols-2 lg:grid-cols-2">
                 <div className="w-full ">
                     <div className="w-full h-64 bg-gray-300 rounded-lg dark:bg-gray-600" />
